3e233105a51de483b1ca56c15e0ad5ef2788d523
tuan <31091365+pinmt@users.noreply.github.com>
2017-08-18 13:05:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 
 
-$dm = 'http://hoctrick.net'; // ko có kí tự / ở cuối nhé
+$dm = 'http://likefb-like-fb.a3c1.starter-us-west-1.openshiftapps.com'; // ko có kí tự / ở cuối nhé
 
 
 $uid = '100003910601531'; // id facebook của bạn
